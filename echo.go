652d5e11bd5427c06a2147bd6968321471f82ef7
Wayne Botha <wayne@witklippies.co.za>
2020-04-05 21:49:15 +0200

    removed logging of service call

--- echo.go
+++ echo.go
@@ -24,12 +24,6 @@ func Echo(w http.ResponseWriter, r *http.Request) {
 	var echoRequest EchoRequest
 	var echoResponse EchoResponse
 
-	//logger.WithFields(logger.Fields{
-	//	"application": "restsvc",
-	//	"service": "echo",
-	//}).Info("echo service")
-	logger.Info("echo service called")
-
 	// Try to decode the request body into the struct. If there is an error,
 	// respond to the client with the error message and a 400 status code.
 	dec := json.NewDecoder(r.Body)
