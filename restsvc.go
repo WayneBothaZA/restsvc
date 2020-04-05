f70535886ab43e8e234a8300043e4f43bd32da34
Wayne Botha <wayne@witklippies.co.za>
2020-04-05 21:15:13 +0200

    Removed echo service calls

--- restsvc.go
+++ restsvc.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"encoding/json"
 	"flag"
 	"fmt"
 	"github.com/gorilla/mux"
@@ -11,68 +10,14 @@ import (
 	"os"
 	"os/signal"
 	"syscall"
-	"time"
 )
 
-const APP_VERSION = "0.0.1 (debug)"
+const APP_VERSION = "0.0.2 (debug)"
 const URL = ":8080"
 
 // The flag package provides a default help printer via -h switch
 var versionFlag *bool = flag.Bool("v", false, "print the version number.")
 var debugFlag *bool = flag.Bool("d", false, "enable debug logging")
-
-type EchoRequest struct {
-	Message string `json:"Message"`
-}
-
-func (p EchoRequest) String() string {
-	return "echo: Message: " + p.Message
-}
-
-type EchoResponse struct {
-	Date    string `json:"Date"`
-	Message string `json:"Message"`
-}
-
-func echo(w http.ResponseWriter, r *http.Request) {
-	var echoRequest EchoRequest
-	var echoResponse EchoResponse
-
-	//logger.WithFields(logger.Fields{
-	//	"application": "restsvc",
-	//	"service": "echo",
-	//}).Info("echo service")
-	logger.Info("echo service called")
-
-	// Try to decode the request body into the struct. If there is an error,
-	// respond to the client with the error message and a 400 status code.
-	dec := json.NewDecoder(r.Body)
-	dec.DisallowUnknownFields()
-
-	err := dec.Decode(&echoRequest)
-	if err != nil {
-		logger.Error(err.Error())
-		http.Error(w, err.Error(), http.StatusBadRequest)
-		return
-	}
-
-	logger.Debug(echoRequest)
-
-	// Return the current date and the original message
-	echoResponse.Date = time.Now().UTC().Format(time.RFC3339)
-	echoResponse.Message = echoRequest.Message
-
-	// Write the response
-	j, err := json.Marshal(echoResponse)
-	if err != nil {
-		logger.Error(err.Error())
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
-	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(http.StatusOK)
-	w.Write(j)
-}
 
 func init() {
 	logger.AddHook(logruseq.NewSeqHook("http://localhost:5341"))
@@ -120,12 +65,9 @@ func main() {
 	// Start signal handler thread
 	go signalHandler(signals)
 
-	//logger.WithFields(logger.Fields{
-	//	"application": "restsvc",
-	//}).Info("Starting REST service" + URL)
 	logger.Info("Starting REST service on " + URL)
 
 	router := mux.NewRouter().StrictSlash(true)
-	router.HandleFunc("/echo", echo).Methods(http.MethodPost)
+	router.HandleFunc("/echo", Echo).Methods(http.MethodPost)
 	logger.Fatal(http.ListenAndServe(URL, router))
 }
