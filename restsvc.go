99dbe482b872ed66ea7c2d48269787a19f09d648
Wayne Botha <wayne@witklippies.co.za>
2020-04-07 19:39:33 +0200

    Added environment variable usage for docker containers

--- restsvc.go
+++ restsvc.go
@@ -13,17 +13,40 @@ import (
 	"syscall"
 )
 
-const APP_VERSION = "0.0.2 (debug)"
-const URL = ":8080"
+const APP_VERSION = "1.0.2"
+const DEFAULT_URL = ":80"
 
 // The flag package provides a default help printer via -h switch
 var versionFlag *bool = flag.Bool("v", false, "print the version number.")
 var debugFlag *bool = flag.Bool("d", false, "enable debug logging")
+var dockerMode *bool = flag.Bool("docker", false, "enable docker mode")
+
+// URL (port) on which to start our listener
+var url string
 
 func init() {
 	// Log to seq instance
-	logger.AddHook(logruseq.NewSeqHook("http://localhost:5341"))
+	seq, found := os.LookupEnv("SEQ")
+	if found {
+		logger.AddHook(logruseq.NewSeqHook(seq))
+	}
 	logger.SetOutput(os.Stdout)
+
+	// set loglevel
+	switch os.Getenv("LOGLEVEL") {
+	case "DEBUG":
+		logger.SetLevel(logger.DebugLevel)
+	case "WARN":
+		logger.SetLevel(logger.WarnLevel)
+	default:
+		logger.SetLevel(logger.InfoLevel)
+	}
+
+	// configure
+	url, found = os.LookupEnv("URL")
+	if !found {
+		url = DEFAULT_URL
+	}
 
 	// Setup signal handler to cleanup properly
 	signals := make(chan os.Signal, 1)
@@ -34,7 +57,7 @@ func init() {
 }
 
 func shutdown() {
-	logger.Info("Stopping REST service on " + URL)
+	logger.Info("Stopping REST service on " + url)
 }
 
 func signalHandler(signals <-chan os.Signal) {
@@ -45,22 +68,19 @@ func signalHandler(signals <-chan os.Signal) {
 }
 
 func main() {
-	flag.Parse() // Scan the arguments list
+	// scan uarguments
+	flag.Parse()
 
-	// Display application version
+	// display application version
 	if *versionFlag {
 		fmt.Println("Version:", APP_VERSION)
 		os.Exit(0)
 	}
 
-	if *debugFlag {
-		logger.SetLevel(logger.DebugLevel)
-		logger.SetReportCaller(true)
-	}
-	logger.Info("Starting REST service on " + URL)
+	logger.Info("Starting REST service on " + url)
 
 	router := mux.NewRouter().StrictSlash(true)
 	router.HandleFunc("/echo", Echo).Methods(http.MethodPost)
 	loggedRouter := handlers.LoggingHandler(logger.StandardLogger().Writer(), router)
-	logger.Fatal(http.ListenAndServe(URL, loggedRouter))
+	logger.Fatal(http.ListenAndServe(url, loggedRouter))
 }
